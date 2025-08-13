45a07fb22d3c98715fd5a118eb1ec7545240c3b6
Сергей <168078160+Elvesto@users.noreply.github.com>
2025-08-13 22:35:44 +0400

    Косметические корректировки

--- SquareSolver.c
+++ SquareSolver.c
@@ -23,24 +23,26 @@ enum NumberOfRoots solverAll(double a, double b, double c, double* x1, double* x
 
 int main() {
     double a, b, c;
-    double x1, x2;
-    scanf("%lf%lf%lf", &a, &b, &c);
-    int numbRoots = solverAll(a, b, c, &x1, &x2);
-    switch (numbRoots) {
-        case ZERO:
-            printf("No real solution");
-            break;
-        case ONE:
-            printf("%lf", x1);
-            break;
-        case TWO:
-            printf("%lf %lf", x1, x2);
-            break;
-        case INFINITE:
-            printf("Infinite solution");
-            break;
-        default:
-            printf("razrab daun");
+    while (1) {
+        scanf("%lf%lf%lf", &a, &b, &c);
+        double x1, x2;
+        int numbRoots = solverAll(a, b, c, &x1, &x2);
+        switch (numbRoots) {
+            case ZERO:
+                printf("No real solution\n");
+                break;
+            case ONE:
+                printf("%lf\n", x1);
+                break;
+            case TWO:
+                printf("%lf %lf\n", x1, x2);
+                break;
+            case INFINITE:
+                printf("Infinite solution\n");
+                break;
+            default:
+                printf("razrab daun");
+        }
     }
 
 }
@@ -51,12 +53,13 @@ double dis(double a, double b, double c) {
 }
 
 void solver1(double a, double b, double* x) {
-    *x = (-b)/2.0/a;
+    *x = (-b) / (2 * a);
 }
 
 void solver2(double a, double b, double D, double* x1, double* x2) {
-    *x1 = (-b + sqrt(D))/2.0/a;
-    *x2 = (-b - sqrt(D))/2.0/a;
+    D = sqrt(D);
+    *x1 = (-b + D)/2.0/a;
+    *x2 = (-b - D)/2.0/a;
 }
 
 bool equalZero(double numb) {
